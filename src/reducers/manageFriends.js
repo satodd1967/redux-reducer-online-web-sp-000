160c6b6482aa92dbab664e43992fa9ad4e992edd
satodd1967 <seth.todd@outlook.com>
2021-02-16 16:42:37 -0600

    commit tree 1d881b78f8f1736720bddbdc5f292d7610289990

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,20 @@
 export function manageFriends(state, action){
 }
+
+function manageFriends(state, action){      
+    switch (action.type) {
+      case 'ADD_FRIEND':
+        return {
+            ...state,
+            friends: [...state.friends, action.friend]
+        }
+      case 'REMOVE_FRIEND':
+          let newFriends = state.friends.filter(friend => friend.id !== action.id)
+        return {
+            ...state,
+            friends: newFriends
+        }
+      default:
+        return state;
+    }
+  }
